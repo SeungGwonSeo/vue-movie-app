66dd6431c2d3ea6f475b503e9fbf442368e804d7
SeungGwonSeo <tjtmdrnjs1@gmail.com>
2021-11-04 17:03:58 +0900

    Create Project

---
+++ src/store/about.js
@@ -0,0 +1,9 @@
+export default {
+    namespaced: true,
+    state: () => ({
+        name: 'SeungGwon',
+        email: 'qkqhtma@naver.com',
+        phone:  '+82-10-9776-3963',
+        image:'https://heropy.blog/css/images/logo.png'
+    })
+}
