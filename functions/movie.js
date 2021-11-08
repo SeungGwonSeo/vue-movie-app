4c80b0d6eb3af0af229105f45fa708dda182e7b0
SeungGwonSeo <tjtmdrnjs1@gmail.com>
2021-11-09 05:53:59 +0900

    Modify server port number

--- functions/movie.js
+++ functions/movie.js
@@ -1,10 +1,10 @@
 const axios = require('axios') // node.js 환경에서는 import 대신 require로 패키지를 불러옴!
+const { OMDB_API_KEY } = process.env
 
 exports.handler = async function (event) {
     console.log(event)
     const payload = JSON.parse(event.body)
     const { title, type, year, page, id } = payload
-    const OMDB_API_KEY = '7035c60c'
     const url = id 
         ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}` 
         : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
